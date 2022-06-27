bdfef684729b0cdd0ee570b867d445920ffecf4e
MiloBg <milofcrs@gmail.com>
2022-06-27 11:50:29 +0200

    Added CRUD operations

---
+++ Codebooks/Controllers/LookupController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codebooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LookupController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LookupMaster>>> Get()
+        {
+            return Ok(await _context.LookupMasters.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<LookupMaster>>> Get(int id)
+        {
+            var lookupMasters = await _context.LookupMasters.Where(m => m.Id == id).Include(v => v.LookupValues).ToListAsync();
+            return Ok(lookupMasters);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<LookupMaster>>> Create(LookupMaster request)
+        {
+            _context.LookupMasters.Add(request);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.LookupMasters.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<LookupMaster>>> DeleteLookupMaster(int id)
+        {
+            var dbLookupMaster = await _context.LookupMasters.FindAsync(id);
+            if (dbLookupMaster == null)
+            {
+                return BadRequest("Lookup not found");
+            }
+            else
+            {
+                _context.LookupMasters.Remove(dbLookupMaster);
+                await _context.SaveChangesAsync();
+                return Ok(await _context.LookupMasters.ToListAsync());
+            }
+        }
+    }
+}
---
+++ Persistence/Migrations/20220627082059_initialcreation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class initialcreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LookupMasters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LookupMasters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LookupValues",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LookupMasterId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LookupValues", x => new { x.LookupMasterId, x.Key });
+                    table.ForeignKey(
+                        name: "FK_LookupValues_LookupMasters_LookupMasterId",
+                        column: x => x.LookupMasterId,
+                        principalTable: "LookupMasters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LookupValues");
+
+            migrationBuilder.DropTable(
+                name: "LookupMasters");
+        }
+    }
+}
